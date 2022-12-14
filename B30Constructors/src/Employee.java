153320c12e77bbc0b45f8de6621efdf469f8337f
23Nikki <kishorenikki2014@gmail.com>
2022-12-14 20:57:42 +0530

    constructors

---
+++ B30Constructors/src/Employee.java
@@ -0,0 +1,23 @@
+class Employee {
+
+int id;
+
+String name;
+
+float salary;
+
+Employee() {
+
+System.out.println("user defined no-argument "
+
++ "constructor executed");
+
+}
+
+void display() {
+
+System.out.println(id + " " + name + " " + salary);
+
+}
+
+}
